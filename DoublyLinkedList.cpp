0603cc103ae05cdfaa2a86f23947e048aacfbe97
ajmalfayiz <ajmal.woodenclouds@gmail.com>
2023-08-04 21:51:10 +0530

    display method added

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -20,20 +20,47 @@ public:
     Node *head = nullptr;
     Node *tail = nullptr;
 
-    void addNode(int data){
+    void addNode(int data)
+    {
         Node *newNode = new Node(data);
-        if(head == nullptr){
+        if (head == nullptr)
+        {
             head = newNode;
-        }else{
+        }
+        else
+        {
             tail->next = newNode;
             newNode->prev = tail;
         }
         tail = newNode;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        if (head == nullptr)
+        {
+            cout << "List is empty\n";
+            return;
+        }
+        while (temp != nullptr)
+        {
+            cout << temp->data << ", ";
+            temp = temp->next;
+        }
     }
 };
 
 int main()
 {
     cout << "Hello Doubly..\n";
+    DoublyLinkedList list;
+    list.display();
+    list.addNode(1);
+    list.addNode(2);
+    list.addNode(3);
+    list.addNode(4);
+    list.addNode(5);
+    list.display();
     return 0;
 }
