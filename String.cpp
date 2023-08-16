6fd265d43b547a51063a76f0b19686cbdc148ac5
ajmalfayiz <talropajmal1016@gmail.com>
2023-08-17 05:24:13 +0530

    String encrypter

---
+++ String.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+void printText(string text, int key)
+{
+    int newKey = key % 26;
+    char newText[text.length()];
+    for (int i = 0; i < text.length(); i++)
+    {
+        int textPosition = text[i] + newKey;
+        if (textPosition <= 122)
+        {
+            newText[i] = static_cast<char>(textPosition);
+        }
+        else
+        {
+            newText[i] = static_cast<char>(96 + textPosition % 122);
+        }
+    }
+    string newString = newText;
+    cout<<newString<<endl;
+}
+
+int main()
+{
+    cout << "Hello world\n";
+    printText("Ajmal",2);
+    return 0;
+}
